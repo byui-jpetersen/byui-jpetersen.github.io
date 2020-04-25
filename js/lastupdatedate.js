4bbbe12928990745e1edd3e184b57af719868370
byui-jpetersen <pet19037@byui.edu>
2020-04-24 20:06:52 -0700

    updated copyright to be dynamic

--- js/lastupdatedate.js
+++
@@ -1,3 +0,0 @@
-//get lasted date it was updated and populate the tag with the lastupdatedate ID
-let oLastModif = new Date(document.lastModified)
-document.getElementById("lastupdatedate").textContent = oLastModif;
