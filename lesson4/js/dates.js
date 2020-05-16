3add7bf728314268275a493792369617c3e7d33e
byui-jpetersen <pet19037@byui.edu>
2020-05-15 22:55:34 -0700

    updates

--- lesson4/js/dates.js
+++ lesson4/js/dates.js
@@ -1,6 +1,8 @@
 //get lasted date it was updated and populate the tag with the lastupdatedate ID
-let oLastModif = new Date(document.lastModified)
-document.getElementById("lastupdatedate").textContent = oLastModif;
+var d = new Date();
+var day = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+var months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+document.getElementById("currentdate").innerHTML = day[d.getDay()] + ", " + d.getDay() + " " + months[d.getMonth()] + " " + d.getFullYear();
 
 //get current year for copyright
 document.getElementById("currentyear").textContent = new Date().getFullYear();
