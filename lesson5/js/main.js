65bcafce731a0ec7964e21043cba8974a6dc1263
byui-jpetersen <pet19037@byui.edu>
2020-05-22 15:53:32 -0700

    updates

--- lesson5/js/main.js
+++ lesson5/js/main.js
@@ -1,23 +1,55 @@
 //get lasted date it was updated and populate the tag with the lastupdatedate ID
 const d = new Date();
-const day = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-var months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
-document.getElementById("currentdate").innerHTML = day[d.getDay()] + ", " + d.getDay() + " " + months[d.getMonth()] + " " + d.getFullYear();
+const day = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+var months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+document.getElementById("currentdate").innerHTML =
+  day[d.getDay()] +
+  ", " +
+  d.getDay() +
+  " " +
+  months[d.getMonth()] +
+  " " +
+  d.getFullYear();
 
 //get current year for copyright
 document.getElementById("currentyear").textContent = new Date().getFullYear();
 
-const hambutton = document.querySelector('.ham');
-const mainnav = document.querySelector('.navigation');
+const hambutton = document.querySelector(".ham");
+const mainnav = document.querySelector(".navigation");
 
-hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+hambutton.addEventListener(
+  "click",
+  () => {
+    mainnav.classList.toggle("responsive");
+  },
+  false
+);
 
 //display banner if it's Friday
-const banner = document.querySelector("pancakes");
-if(d.getDay() == 3) {
-    banner.style.display = "block";
+const banner = document.getElementById("pancakes");
+if (d.getDay() == 5) {
+  banner.style.display = "block";
+} else {
+  banner.style.display = "none";
 }
-else {
-    banner.style.display = "none";
-}
-
