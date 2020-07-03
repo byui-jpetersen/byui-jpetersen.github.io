d55958455c02a1a63c2e3f1e76105f374067c4e9
byui-jpetersen <pet19037@byui.edu>
2020-07-02 22:55:00 -0700

    changes

--- lesson11/js/fhevents.js
+++ lesson11/js/fhevents.js
@@ -1,39 +1,35 @@
-const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+const requestURL = "https://byui-cit230.github.io/weather/data/towndata.json";
 
 fetch(requestURL)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (jsonObject) {
-        //console.table(jsonObject); // temporary checking for valid response and data parsing
-        const towns = jsonObject['towns'];
-        const southern = towns.filter(town => (town.name == 'Fish Haven'));
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    const towns = jsonObject["towns"];
+    const southern = towns.filter((town) => town.name == "Fish Haven");
 
-        southern.forEach(town => {
+    southern.forEach((town) => {
+      let eachTown = document.createElement("article");
+      let h3 = document.createElement("h3");
+      let p1 = document.createElement("p");
+      let p2 = document.createElement("p");
+      let p3 = document.createElement("p");
+      let p4 = document.createElement("p");
 
-            let eachTown = document.createElement('article');
-            let h3 = document.createElement('h3');
-            let p1 = document.createElement('p');
-            let p2 = document.createElement('p');
-            let p3 = document.createElement('p');
-            let p4 = document.createElement('p');
-            let p5 = document.createElement('p');
+      eachTown.appendChild(p1);
 
-            eachTown.appendChild(p1)
+      h3.innerHTML = `Upcoming Events`;
+      p1.appendChild(h3);
 
-            h3.innerHTML = `Upcoming Events`;
-            p1.appendChild(h3);
+      p2.innerHTML = `${town.events[0]}`;
+      p1.appendChild(p2);
 
-            p2.innerHTML = `${town.events[0]}`;
-            p1.appendChild(p2);
+      p3.innerHTML = `${town.events[1]}`;
+      p1.appendChild(p3);
 
-            p3.innerHTML = `${town.events[1]}`;
-            p1.appendChild(p3);
+      p4.innerHTML = `${town.events[2]}`;
+      p1.appendChild(p4);
 
-            p4.innerHTML = `${town.events[2]}`;
-            p1.appendChild(p4);
-
-            document.querySelector('section.fishhavenevents').appendChild(eachTown);
-        });
-
+      document.querySelector("section.fishhavenevents").appendChild(eachTown);
     });
+  });
